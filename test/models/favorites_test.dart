ac0852f46104c1cb9a4ed2add53cadbc8bce2cba
mi11km <mi11km.basket@gmail.com>
2021-09-17 00:35:43 +0900

    refactoring

--- test/models/favorites_test.dart
+++ test/models/favorites_test.dart
@@ -2,9 +2,9 @@ import 'package:test/test.dart';
 import 'package:startup_namer/models/favorites.dart';
 
 void main() {
-  group('App Provider Tests', () {
+  group('App Provider Tests.', () {
     var favorites = Favorites();
-    test('A new item should be added', () {
+    test('A new item should be added.', () {
       var number = 35;
       favorites.add(number);
       expect(favorites.items.contains(number), true);
